ffab2e10859669b265b969b2a154d0eff2339251
SergeyNovikLV <mr.noviks@gmail.com>
2020-05-20 10:46:14 +0300

    выполнил 8. Стороны прямоугольника. [#289566]

---
+++ src/ru/job4j/condition/SqArea.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+/*
+Допишите метод square.
+2. Доработайте метод main с демонстрацией работы программы с параметрами p = 6, k = 2, площадь должна быть 2.
+h = p / (2 * (k + 1));
+L = h * k;
+s = L * h;
+ */
+public class SqArea {
+    public static double square(int p, int k) {
+        double rsl = k * (p / (2 * (k + 1))) * p / (2 * (k + 1));
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double result1 = SqArea.square(6, 2);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+    }
+}
