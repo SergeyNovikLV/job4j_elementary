979c52a4ea86270b14bedf3f18f7f939b4c275f9
SergeyNovikLV <mr.noviks@gmail.com>
2020-05-20 11:10:51 +0300

    выполнил 9.1. String [#289573]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+/*
+- создайте переменную с именем idea типа String и присвойте ей значение "I like Java!";
+
+   - выведите на консоль эту переменную через команду System.out.println(idea);
+
+   - присоедините к переменной idea строку "But I am a newbie.";
+
+   - создайте переменную year с типом int и присвойте ей текущий год.
+
+   - присоедините к переменной idea переменную year;
+
+   - выведите на консоль эту переменную через команду System.out.println(idea);
+ */
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        int year = 2020;
+        System.out.println(idea + " " + "But I am a newbie." + " " + year);
+    }
+}
