13c36621b4c91bc07323cbf51bc12e11db4de97c
SergeyNovikLV <mr.noviks@gmail.com>
2020-05-26 12:38:20 +0300

    удалил лишнюю часть кода 2.3.Тесты для определения расстояния между точками.[159459#289600]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -11,7 +11,6 @@ public class PointTest {
         int in2 = 2;
         int in3 = 0;
         int in4 = 2;
-        double rsl = Math.pow((in2 - in1), 2) - Math.pow((in4 - in3), 2);
         double expected = 4.0;
         double out = Point.distance(0, 0, 2, 0);
         Assert.assertEquals(expected, out, 0.01);
