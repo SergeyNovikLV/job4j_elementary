73aaa884eb74d482e9b01703d0ddb1d2dfd0080d
SergeyNovikLV <mr.noviks@gmail.com>
2020-06-02 10:31:20 +0300

    выполнил 5.8. JavaDoc. Code convention [#289623]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,29 +1,38 @@
 package ru.job4j.converter;
-/*
 
-Задание.
-
-1. Реализовать метод конвертации рублей в евро и доллары.
-
-Вам нужно заменить -1 на формулу для вычисления валюты.
-
-1 доллар = 60 рублей.
-1 евро = 70 рублей.
-
-2. Доработайте метод main. Добавьте в него примеры выводов всех видов конвертаций.
+/**
+ * Class Converter решение задачи Ном. 5. Конвертер валюты  из первого раздела Тип данных
+ * @author novik
+ * @since 20.05.2020
+ * @version 1
 */
 
 public class Converter {
 
+    /**
+     * Method rubleToEuro.
+     * @param value - value.
+     * @return rsl - результат конвертации в евро.
+     */
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
 
+    /**
+     * Method rubleToDollar.
+     * @param value - value.
+     * @return rsl - результат конвертации в доллары.
+     */
     public static int rubleToDollar(int value) {
-        int rsl = value / 60; // формула перевода рублей в доллоры.
+        int rsl = value / 60;
         return rsl;
     }
+
+    /**
+     * Method main
+     * @param args - args.
+    */
 
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
@@ -31,13 +40,26 @@ public class Converter {
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("140 rubles are " + dollar + " dollars.");
 
-        // Euro convertation test
+        /*
+         * переменная для проверки корректности конвертации в евро.
+         * переменная in - входные данные количество рублей.
+         * переменная expectedEuro -ожидаемое значение количество евро.
+         * переменная out -результат работы программы.
+         * переменная passedRblTest - сравнивает  ожидаемое значение с результатом вычеслений.
+         */
         int inRbl = 140;
         int expectedEuro = 2;
         int out = rubleToEuro(inRbl);
         boolean passedRblTest = expectedEuro == out;
         System.out.println("140 rubles are 2. Test result : " + passedRblTest);
-        // Dollar convertation test
+
+        /*
+         * Тестовый код для проверки корректности конвертации в доллары.
+         * переменная in - входные данные количество рублей.
+         * переменная expectedEuro -ожидаемое значение количество долларах.
+         * переменная out -результат работы программы.
+         * переменная passedRblTest - сравнивает  ожидаемое значение с результатом вычеслений.
+         */
         int inRbl2 = 140;
         int expectedDollar = 2;
         int out2 = rubleToDollar(inRbl2);
