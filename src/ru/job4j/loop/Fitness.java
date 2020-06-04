deb4efb1c2457cae47162652240be73733bada4a
SergeyNovikLV <mr.noviks@gmail.com>
2020-06-04 20:28:25 +0300

    исправил 5.4. Протеиновая диета. [#289612]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -23,7 +23,7 @@ import ru.job4j.condition.Cinema;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan < nik) {
+        while (ivan <= nik) {
             ivan *= 3;
             nik *= 2;
             month += 1;
