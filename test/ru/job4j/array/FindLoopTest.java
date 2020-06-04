8f25151c18447cfd06f06469d547d7c1e1cfd655
SergeyNovikLV <mr.noviks@gmail.com>
2020-06-04 13:11:22 +0300

    выполнил 6.1. Классический поиск перебором. [#289537]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -25,4 +25,14 @@ public class FindLoopTest {
         int expect = 4;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenArrayHas20Then5() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3, 12, 16, 18};
+        int value = 20;
+        int result = find.indexOf(input, value);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
 }
