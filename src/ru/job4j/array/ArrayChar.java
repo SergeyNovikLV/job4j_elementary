3c2660343968fb1c75b220889ddc3130f657c65f
SergeyNovikLV <mr.noviks@gmail.com>
2020-06-10 08:58:39 +0300

    выполнил 6.4. Слово начинается с ... [#289539]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -1,20 +1,15 @@
 package ru.job4j.array;
 import java.util.Arrays;
-/*
-Метод принимает два массива символов.
-В цикле for вам нужно сравнить значения ячеек с одинаковым индексом.
-Если все ячейки из массива  соответствуют таким же ячейкам в массиве word, значит, нужно вернуть true.
- index
- */
+
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int index = 0; index < pref.length; index++) {
-         if (word[index] == pref[index]) {
+         if (pref[index] == word[index]) {
              result = true;
          } else {
-             result = Arrays.equals(word, pref);
+             result = false;
          }
         }
         return result;
