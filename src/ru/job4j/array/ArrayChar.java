41ebf4b3b7f9ed66ca5fc07c44a4ec8eced88334
SergeyNovikLV <mr.noviks@gmail.com>
2020-06-11 10:12:02 +0300

    подправил 6.4. Слово начинается с ... [#289539]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -6,11 +6,10 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int index = 0; index < pref.length; index++) {
-         if (pref[index] == word[index]) {
-             result = true;
-         } else {
-             result = false;
-         }
+            if (pref[index] != word[index]) {
+                result = false;
+                break;
+            }
         }
         return result;
     }
